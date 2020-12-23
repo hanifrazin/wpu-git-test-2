25bf18c4689cac67da1ee649dc44bbb0dab09fbf
Hanif Razin <hanifrazin@gmail.com>
2020-12-23 11:38:13 +0700

    tambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo omnis magni totam rem blanditiis fuga aspernatur sed cupiditate quos provident id sint eos cum repellendus ipsam veniam, non accusamus modi.
+    Eum mollitia omnis sapiente veritatis non sunt facilis eos eveniet recusandae amet, architecto, rerum, nisi pariatur quae. Distinctio ullam dolorem magnam recusandae natus suscipit, doloremque, repellendus debitis quod eligendi unde!
+    Rem repudiandae adipisci corrupti voluptatum cupiditate voluptas quidem, consequuntur corporis laborum molestias officiis laudantium veritatis quam, sunt quis? Ipsum, eum. Repellendus assumenda, expedita officia voluptatem corporis quos saepe perferendis facilis?</p>
 </body>
 </html>
